9b77e60f775f3b7fb5d0cdc1fedfd5f0ca4cde85
martinaangeles <121085579+martinaangeles@users.noreply.github.com>
2023-11-25 22:11:21 +0800

    Delete src directory

--- src/components/Home.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="bg-blue-900 w-screen h-screen">
-      <div className="text-center w-full">
-        <button className="bg-white p-2 rounded-lg"> Login with Google </button>
-      </div>
-    </div>
-  );
-}
